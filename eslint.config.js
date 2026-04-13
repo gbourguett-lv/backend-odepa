4ba8f8249258627b4d55b66ad7ca707cb8aff355
Gerard Bourguett <gbourguett@lovalledor.cl>
2026-04-13 15:16:22 -0400

    ci: migrate ESLint to v9 flat config
    ESLint 9 no longer supports .eslintrc.* files. Migrate to
    eslint.config.js (flat config) with equivalent rules.
    Update lint script to drop deprecated --ext flag.

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
+  },
+  {
+    files: ['src/**/*.ts', '__tests__/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'warn',
+    },
+  },
+  prettierConfig,
+];
